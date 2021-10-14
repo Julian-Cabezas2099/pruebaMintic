54483d8a2a6119d9a65010da772f4390bce81ad2
julian-Cabezas20 <julian.cabezas.mt@correo.usa.edu.co>
2021-10-13 21:35:56 -0500

    Este es el primer intento

---
+++ src/main/java/com/pruebaMintic/springboot/app/PruebaMinticApplication.java
@@ -0,0 +1,13 @@
+package com.pruebaMintic.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaMinticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaMinticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebaMintic/springboot/app/PruebaMinticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebaMintic.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaMinticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
